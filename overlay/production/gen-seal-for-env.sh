0501a2fce96fb7acbe4b2ebe3d81dbfda10e42c6
Phuc Phung <phungthienphuc18@gmail.com>
2022-07-11 17:36:47 +0700

    init repo for tribes ops

---
+++ overlay/production/gen-seal-for-env.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e 
+SECRET_NAME=$1
+RESULT_DIRECTORY=${2:-.}
+
+# if [ $# -ne 1 ]; then
+#     echo $0: usage: $0 SECRET_NAME
+#     exit 1
+# fi
+
+kubectl create secret -n production generic $SECRET_NAME --dry-run=client --from-file=env=configs/env -o yaml
